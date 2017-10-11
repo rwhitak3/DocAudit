4ac10f5b2ae0ea97d800b8d3825e1f88b868fc36
Robert Whitaker <rwhitak3@spsu.edu>
2017-10-10 22:57:11 -0400

    Added image pdf, and image processing. via tesseract.
    on ubuntu:
    $ sudo apt-get install tesseract-ocr tesseract-ocr-eng
    $ export TESSDATA_PREFIX=/usr/share/tesseract-ocr/tessdata/
    $ tesseract --list-langs
    
    
    before building.

---
+++ src/main/java/edu/kennesaw/cs4850/docaudit/convert/ImageToText.java
@@ -0,0 +1,11 @@
+package edu.kennesaw.cs4850.docaudit.convert;
+
+import java.awt.image.BufferedImage;
+
+/**
+ *
+ * @author rwhitak3
+ */
+public interface ImageToText {
+    public String convert(BufferedImage img);
+}
--- src/main/java/edu/kennesaw/cs4850/docaudit/ingest/ReadPDFPDFBox.java
+++ src/main/java/edu/kennesaw/cs4850/docaudit/ingest/ReadPDFPDFBox.java
@@ -1,5 +1,6 @@
 package edu.kennesaw.cs4850.docaudit.ingest;
 
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -9,8 +10,13 @@ import org.apache.pdfbox.cos.COSDocument;
 import org.apache.pdfbox.io.RandomAccessFile;
 import org.apache.pdfbox.pdfparser.PDFParser;
 import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.rendering.ImageType;
+import org.apache.pdfbox.rendering.PDFRenderer;
 import org.apache.pdfbox.text.PDFTextStripper;
 import org.slf4j.LoggerFactory;
+import edu.kennesaw.cs4850.docaudit.convert.ImageToText;
+import edu.kennesaw.cs4850.docaudit.convert.ImageToTextTesseract;
 
 /**
  *
@@ -19,9 +25,7 @@ import org.slf4j.LoggerFactory;
 public class ReadPDFPDFBox implements ReadPDF {
     private org.slf4j.Logger logger = LoggerFactory.getLogger(ReadPDFPDFBox.class);
     private PDFParser parser;
-    private PDFTextStripper pdfStripper;
-    private PDDocument pdDoc ;
-    private COSDocument cosDoc ;
+
    
     @Override
     public String readPDF(File inputFile) {
@@ -30,15 +34,15 @@ public class ReadPDFPDFBox implements ReadPDF {
             this.parser = new PDFParser(new RandomAccessFile(inputFile,"r")); // update for PDFBox V 2.0
             
             parser.parse();
-            cosDoc = parser.getDocument();
-            pdfStripper = new PDFTextStripper();
-            pdDoc = new PDDocument(cosDoc);
-            pdfStripper.setStartPage(1);
-            pdfStripper.setEndPage(pdDoc.getNumberOfPages());
-            
-            String output = pdfStripper.getText(pdDoc);
-            logger.debug("Output: " + output);
-            return output;
+            String textOutput = readCosDoc(parser.getDocument());
+            String imgOutput = "";
+            if ( textOutput.length() < 20 ) {
+                imgOutput = readImages(parser.getPDDocument());
+            }
+            if (imgOutput.length() > 20) {
+                textOutput = imgOutput;
+            }
+            return textOutput;
             
         } catch (FileNotFoundException ex) {
             logger.error("File Not found", ex);
@@ -49,4 +53,37 @@ public class ReadPDFPDFBox implements ReadPDF {
         }
     }
     
+    private String readCosDoc(COSDocument cosDoc) throws IOException {
+        PDFTextStripper pdfStripper = new PDFTextStripper();
+
+        PDDocument pdDoc = new PDDocument(cosDoc);
+        pdfStripper.setStartPage(1);
+        pdfStripper.setEndPage(pdDoc.getNumberOfPages());
+
+        String output = pdfStripper.getText(pdDoc);
+        logger.debug("Output: " + output);
+        return output;    
+    }
+
+    private String readImages(PDDocument pdDoc) {
+        PDFRenderer r = new PDFRenderer(pdDoc);
+        int pageNumber = pdDoc.getNumberOfPages();
+        ImageToText conv = new ImageToTextTesseract();
+        StringBuilder finalOutput = new StringBuilder();
+        try {
+            for ( int i = 0; i < pageNumber; i++ ) {
+                logger.debug("Reading Page " + Integer.toString(i));
+                BufferedImage image = r.renderImageWithDPI(i, 300);
+                String out = conv.convert(image);
+                logger.debug("Got : " + out);
+                finalOutput.append(out);
+                return finalOutput.toString();
+            }
+            logger.debug("Output: " + finalOutput.toString());
+            return finalOutput.toString();
+        }  catch (IOException ex) {
+            logger.error("IO Exception while trying to view images in pdf", ex);
+            return null;
+        }
+    }
 }
